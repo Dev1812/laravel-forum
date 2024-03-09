beeff65cea56cbdc85fe46b9e99f24c81efa79e6
timur <timka_issaev@mail.ru>
2024-03-09 18:15:52 +0300

    added new response pathes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Http\Request;
 
-define('SITE_NAME', 'Laravel-Market');
+define('SITE_NAME', 'Laravel-Forum');
 define('SITE_ROOT', str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']).'/');
 
 include_once SITE_ROOT.'/common.php';
@@ -11,14 +11,12 @@ Route::match(['get', 'post'], '/', function() {
 });
 
 Route::match(['get', 'post'], '/feed', function() {
-  return redirect('/feed');
+  return view('/feed.feed');
 });
 
 Route::match(['get', 'post'], '/logout', function() {
     Session::flush();
     return redirect('/');
-
-
 });
 
 Route::get('/reg', 'RegController@index');
@@ -28,3 +26,15 @@ Route::get('/login', 'LoginController@index');
 Route::post('/login', 'LoginController@index');
 
 Route::get('/id{profile_id}', 'ProfileController@index');
+
+
+Route::get('/create_question', 'QuestionController@createQuetion');
+
+
+Route::get('/reply_to_question', 'QuestionController@replyToQuestion');
+
+
+Route::get('/reply_to_question', 'QuestionController@replyToQuestion');
+
+
+
